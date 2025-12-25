1280eac1560464047bed47134c116edbd8d333a5
dhruvaacharyacs24-repo <dhruvaacharya.cs24@bmsce.ac.in>
2025-12-25 16:39:52 +0530

    Add files via upload

---
+++ prg2.c
@@ -0,0 +1,59 @@
+/*#include <stdio.h>
+#include <string.h>
+
+char stack[100];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char c) {
+    if (c == '+' || c == '-') return 1;
+    if (c == '*' || c == '/') return 2;
+    return 0;
+}
+
+int main() {
+    char infix[100], postfix[100];
+    int i, k = 0;
+    char ch;
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; i < strlen(infix); i++) {
+        ch = infix[i];
+
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+            postfix[k++] = ch;
+        } else if (ch == '(') {
+            push(ch);
+        } else if (ch == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[k++] = pop();
+            }
+            pop();
+        } else {
+            while (top != -1 && precedence(stack[top]) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}*/
+
